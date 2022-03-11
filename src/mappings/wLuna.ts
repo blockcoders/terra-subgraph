26c7fd74dd114cb025b7e9d6ebaa911fa8d51586
Fernando Sirni <fsirni@gmail.com>
2022-03-11 01:29:12 -0300

    Changes on mappings

--- src/mappings/wLuna.ts
+++ src/mappings/wLuna.ts
@@ -6,88 +6,102 @@ import {
 } from '../../generated/wLuna/wLuna'
 import { Approval, Burn, OwnershipTransferred, Transfer, Token, User } from '../../generated/schema'
 
+const LUNA = "LUNA"
+//const LUNA_ADDRESS = '0x320bc76961fb4e2a0e2e86d43d4b9d13b4985b8f'
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
 export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
+  let entity = new Approval(event.transaction.hash.toHex())
   entity.owner = event.params.owner
   entity.spender = event.params.spender
-  entity.value = event.params.value
+  entity.amount = event.params.value
   entity.save()
 }
 
-export function handleBurn(event: BurnEvent): void {
-  const id = 'LUNA'
-  let wLuna = Token.load(id)
-  if(!wLuna) {
-    wLuna = new Token(id)
-  }
-  wLuna.amount = wLuna.amount.minus(event.params.amount)
-  wLuna.totalBurned = wLuna.totalBurned.plus(event.params.amount)
-
-  const userId = event.params._sender.toHex()
-  let user = User.load(userId)
-  if(!user) {
-    user = new User(userId)
-  }
-  user.wLunaBalance = user.wLunaBalance.minus(event.params.amount)
-  user.txCount = user.txCount + 1
-
-  const burnId = event.transaction.hash.toHex() + '-' + event.logIndex.toString()
-  const burn = new Burn(burnId)
-  burn.timestamp = event.block.timestamp.toI32()
-  burn.burner = userId
-  burn.amount = event.params.amount
-
-  wLuna.save()
-  user.save()
-  burn.save()
-}
-
 export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
-  let entity = new OwnershipTransferred(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
+  let entity = new OwnershipTransferred(event.transaction.hash.toHex())
   entity.previousOwner = event.params.previousOwner
   entity.newOwner = event.params.newOwner
   entity.save()
 }
 
+const getToken = (tokenName: string): Token => {
+  let token = Token.load(tokenName)
+  if (!token) {
+    token = new Token(tokenName)
+  }
+  return token
+}
+
+export function handleBurn(event: BurnEvent): void {
+  const token = getToken(LUNA)
+  const burnAmount = event.params.amount
+  //token.amount = token.amount.minus(burnAmount)
+  token.totalBurned = token.totalBurned.plus(burnAmount)
+
+  const userId = event.params._sender.toHex()
+  
+  const burnId = event.transaction.hash.toHex()
+  const burn = new Burn(burnId)
+  burn.timestamp = event.block.timestamp.toI32()
+  burn.burner = userId
+  burn.amount = burnAmount
+  burn.save()
+
+  let user = User.load(userId)
+  if (!user) {
+    user = new User(userId)
+    token.totalUsersEver = token.totalUsersEver + 1
+  }
+  user.txCount = user.txCount + 1
+  user.save()
+
+  /*let lunaUser = User.load(LUNA_ADDRESS)
+  if (lunaUser) {
+    lunaUser.lunaBalance = lunaUser.lunaBalance.minus(burnAmount)
+    lunaUser.save()
+  }*/
+  token.save()
+}
+
 export function handleTransfer(event: TransferEvent): void {
-  const id = 'LUNA'
-  let wLuna = Token.load(id)
-  if(!wLuna) {
-    wLuna = new Token(id)
-  }
-
+  const token = getToken(LUNA)
   const userToId = event.params.to.toHex()
-  let userTo = User.load(userToId)
-  if (!userTo) {
-    userTo = new User(userToId)
-    userTo.transferFrom = []
-    userTo.transfersTo = []
-    userTo.burns = []
-    wLuna.totalUsersEver = wLuna.totalUsersEver + 1
-  }
-  userTo.wLunaBalance = userTo.wLunaBalance.plus(event.params.value)
-
   const userFromId = event.params.from.toHex()
+  const transferId = event.transaction.hash.toHex()
+  const txAmount = event.params.value
+
+  const transfer = new Transfer(transferId)
+  transfer.from = userFromId
+  transfer.to = userToId
+  transfer.amount = txAmount
+  transfer.timestamp = event.block.timestamp.toI32()
+  transfer.save()
+
+  /*if (userToId == LUNA_ADDRESS) { // Mint
+    token.amount = token.amount.plus(txAmount)
+  }*/
+
   let userFrom = User.load(userFromId)
   if (!userFrom) {
     userFrom = new User(userFromId)
-    userFrom.transferFrom = []
-    userFrom.transfersTo = []
-    userFrom.burns = []
-    wLuna.totalUsersEver = wLuna.totalUsersEver + 1
+    token.totalUsersEver = token.totalUsersEver + 1
   }
-  userFrom.wLunaBalance = userFrom.wLunaBalance.minus(event.params.value)
+  if (userFromId != ZERO_ADDRESS) {
+    userFrom.lunaBalance = userFrom.lunaBalance.minus(txAmount)
+  }
   userFrom.txCount = userFrom.txCount + 1
-
-  const transferd = event.transaction.hash.toHex() + '-' + event.logIndex.toString()
-  const transfer = new Transfer(transferd)
-  transfer.from = userFromId
-  transfer.to = userToId
-  transfer.amount = event.params.value
-  transfer.timestamp = event.block.timestamp.toI32()
-
-  transfer.save()
-  wLuna.save()
-  userTo.save()
   userFrom.save()
+
+  let userTo = User.load(userToId)
+  if (!userTo) {
+    userTo = new User(userToId)
+    token.totalUsersEver = token.totalUsersEver + 1
+  }
+  if (userToId != ZERO_ADDRESS) {
+    userTo.lunaBalance = userTo.lunaBalance.plus(txAmount)
+  }
+  userTo.save()
+
+  token.save()
 }
