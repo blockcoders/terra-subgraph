aba9402f35fb164b4dc04385e239c0c5e9fbeb0a
Fernando Sirni <33009762+fersirni@users.noreply.github.com>
2022-03-25 14:55:06 -0300

    Subgraph for Terra (wLuna - wUST) (#1)
    * Adding subgraph for wLuna - first commit
    
    * adding wUST subgraph
    
    * adding mustache to swtich ropsten to mainnet
    
    * Adding entities User and Token
    
    * fix reference
    
    * adding start block numbers to config
    
    * Changes on mappings
    
    * Cleaning code
    
    * push generated
    
    * Adding some details to the readme
    
    * fix build
    
    * updated dependencies
    
    * create package lock again
    
    * fix pipeline
    
    * pipeline
    
    Co-authored-by: Jose Ramirez <jarcodallo@gmail.com>

--- README.md
+++ README.md
@@ -14,11 +14,98 @@ Below are a few ways to show how to query the terra-subgraph for data. The queri
 
 ## Entity Overviews
 
-**PENDING**
+### Token
+
+- **id**: Token symbol (LUNA-UST)
+- **totalBurned**: Total amount of burned tokens
+- **totalUsersEver**: Total amount of created users that have sent, received or burned tokens
+
+### Transfer
+
+- **id**: Transaction hash
+- **timestamp**: Block timestamp of the event
+- **from**: User that represents the sender of the transaction
+- **to**: User that represents the receiver of the transaction
+- **amount**: Amount in wei sent by the "from" User
+
+### User
+
+- **id**: Represents the address of the user
+- **lunaBalance**: Balance of wLUNA tokens in wei
+- **ustBalance**: Balance of wUST tokens in wei
+- **txCount**: Increments by one every time the user makes a transaction
+
+### Approval
+
+- **id**: Transaction hash
+- **owner**: Address of the tokens's owner
+- **spender**: Address of the tokens's spender
+- **amount**: Amount of tokens to approve
+
+### Burn
+
+- **id**: Transaction hash
+- **timestamp**: Block timestamp of the event
+- **burner**: User that reprents the sender of the Burn transaction
+- **amount**: Amount of tokens to be burned
+
+### OwnershipTransferred
+
+- **id**: Transaction hash
+- **previousOwner**: Address of the previous owner
+- **newOwner**: Address of the new owner
 
 ## Example Queries
 
-**PENDING**
+```
+{
+    users(first: 5) {
+        id
+        lunaBalance
+        ustBalance
+        txCount
+    }
+    tokens(first: 2) {
+        id
+        totalBurned
+        totalUsersEver
+    }
+    transfers(first: 10) {
+        id
+        timestamp
+        amount
+        from {
+            id
+            ustBalance
+            lunaBalance
+            txCount
+        }
+        to {
+            id
+            txCount
+            ustBalance
+            lunaBalance
+        }
+    }
+    approvals(first: 5) {
+        id
+        owner
+        spender
+        amount
+    }
+    burns(first: 5) {
+        id
+        timestamp
+        burner
+        amount
+    }
+    ownershipTransferreds(first: 5) {
+        id
+        previousOwner
+        newOwner
+    }
+}
+```
 
 ## Change Log
 
@@ -30,6 +117,7 @@ Contributions welcome! See [Contributing](CONTRIBUTING.md).
 
 ## Collaborators
 
+- [**Fernando Sirni**](https://github.com/fersirni)
 - [**Jose Ramirez**](https://github.com/0xslipk)
 
 ## License
---
+++ src/mappings/ust.ts
@@ -0,0 +1,98 @@
+import {
+  Approval as ApprovalEvent,
+  Burn as BurnEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent,
+} from '../types/UST/UST'
+import { Approval, Burn, OwnershipTransferred, Transfer, Token, User } from '../types/schema'
+
+const UST = "UST"
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(event.transaction.hash.toHex())
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.amount = event.params.value
+  entity.save()
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
+  let entity = new OwnershipTransferred(event.transaction.hash.toHex())
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+  entity.save()
+}
+
+const getToken = (tokenName: string): Token => {
+  let token = Token.load(tokenName)
+  if (!token) {
+    token = new Token(tokenName)
+  }
+  return token
+}
+
+export function handleBurn(event: BurnEvent): void {
+  const token = getToken(UST)
+  const burnAmount = event.params.amount
+  token.totalBurned = token.totalBurned.plus(burnAmount)
+
+  const userId = event.params._sender.toHex()
+  
+  const burnId = event.transaction.hash.toHex()
+  const burn = new Burn(burnId)
+  burn.timestamp = event.block.timestamp.toI32()
+  burn.burner = userId
+  burn.amount = burnAmount
+  burn.save()
+
+  let user = User.load(userId)
+  if (!user) {
+    user = new User(userId)
+    token.totalUsersEver = token.totalUsersEver + 1
+  }
+  user.txCount = user.txCount + 1
+  user.save()
+
+  token.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  const token = getToken(UST)
+  const userToId = event.params.to.toHex()
+  const userFromId = event.params.from.toHex()
+  const transferId = event.transaction.hash.toHex()
+  const txAmount = event.params.value
+
+  const transfer = new Transfer(transferId)
+  transfer.from = userFromId
+  transfer.to = userToId
+  transfer.amount = txAmount
+  transfer.timestamp = event.block.timestamp.toI32()
+  transfer.save()
+
+  let userFrom = User.load(userFromId)
+  if (!userFrom) {
+    userFrom = new User(userFromId)
+    token.totalUsersEver = token.totalUsersEver + 1
+  }
+  if (userFromId != ZERO_ADDRESS) {
+    userFrom.ustBalance = userFrom.ustBalance.minus(txAmount)
+  }
+  userFrom.txCount = userFrom.txCount + 1
+  userFrom.save()
+
+  let userTo = User.load(userToId)
+  if (!userTo) {
+    userTo = new User(userToId)
+    if (userToId != ZERO_ADDRESS) {
+      token.totalUsersEver = token.totalUsersEver + 1
+    }
+  }
+  if (userToId != ZERO_ADDRESS) {
+    userTo.ustBalance = userTo.ustBalance.plus(txAmount)
+  }
+  userTo.save()
+
+  token.save()
+}
